e68c4e2722c9ff723cfbc03853f70de4ac5534a7
infrxion <52427202+infrxion@users.noreply.github.com>
2021-08-06 10:48:05 -0600

    Add project files.

---
+++ MarbleSolitaire/MarbleSolitaire.cpp
@@ -0,0 +1,215 @@
+#include <iostream>
+#include <vector>
+#include <array>
+#include <string>
+#include <windows.h>
+
+using namespace std;
+
+// Dimensions of the board
+const int BOARD_SIZE = 7;
+// Number of moves required to solve
+const int MOVE_COUNT = 35;
+
+typedef int board_state[BOARD_SIZE][BOARD_SIZE];
+
+board_state initial_data = {
+    { -1,-1, 1, 1, 1,-1,-1 },
+    { -1, 1, 1, 1, 1, 1,-1 },
+    {  1, 1, 1, 1, 1, 1, 1 },
+    {  1, 1, 1, 0, 1, 1, 1 },
+    {  1, 1, 1, 1, 1, 1, 1 },
+    { -1, 1, 1, 1, 1, 1,-1 },
+    { -1,-1, 1, 1, 1,-1,-1 }
+};
+
+enum Dir { Up, Down, Left, Right };
+
+struct Move {
+    int x;
+    int y;
+    Dir dir;
+
+    explicit operator string() {
+        return to_string(y) + string(", ") + to_string(x) + string(" ") + string(dir == Up ? "\x1e" : dir == Right ? "\x10" : dir == Down ? "\x1f" : "\x11");
+    }
+
+    // Returns the move passed in with the coordinates incremented in the direction specified in the move.
+    // This may return an out-of-bounds coordinate.
+    Move increment_direction();
+};
+
+Move Move::increment_direction() {
+    Move m;
+    m.dir = dir;
+    m.x = x;
+    m.y = y;
+    switch (dir) {
+    case Up:
+        m.x--;
+        break;
+    case Right:
+        m.y++;
+        break;
+    case Down:
+        m.x++;
+        break;
+    case Left:
+        m.y--;
+        break;
+    };
+    return m;
+};
+
+typedef array<Move, MOVE_COUNT> solution;
+
+
+int searched_amount = 0;
+
+
+
+
+// Defines a board state with functions 
+class Board {
+public:
+    Board(board_state state);
+    void log();
+    void solve(vector<solution> &output, solution stack, int search_level);
+    bool make_move(Move &mov);
+private:
+    board_state state;
+};
+
+Board::Board(board_state copy_state) {
+    for (int x = 0; x < BOARD_SIZE; x++) {
+        for (int y = 0; y < BOARD_SIZE; y++) {
+            state[x][y] = copy_state[x][y];
+        }
+    }
+}
+
+void Board::log() {
+    for (int i = 0; i < BOARD_SIZE; i++) {
+        for (int j = 0; j < BOARD_SIZE; j++) {
+            switch (state[i][j]) {
+            case -1:
+                cout << "  ";
+                break;
+            case 0:
+                cout << "\xb0\xb0";
+                break;
+            case 1:
+                cout << "\xb2\xb2";
+                break;
+            }
+        }
+        cout << "\n";
+    }
+}
+
+// Attempts to modify the board state to reflect the move passed in. If successful, will return 
+bool Board::make_move(Move &m0) {
+    Move m1 = m0.increment_direction();
+    Move m2 = m1.increment_direction();
+
+    bool oob = false;
+
+    switch (m0.dir) {
+    case Up:
+        oob = m2.x < 0;
+        break;
+    case Right:
+        oob = m2.y >= BOARD_SIZE;
+        break; 
+    case Down:
+        oob = m2.x >= BOARD_SIZE;
+        break; 
+    case Left:
+        oob = m2.y < 0;
+        break;
+    }
+
+
+    // This will validate whether a move is possible: the origin marble should be there, the marble being jumped over should
+    // be there, and there should be an empty space in the destination.
+    if (
+        oob ||
+        state[m0.x][m0.y] != 1 ||
+        state[m1.x][m1.y] != 1 ||
+        state[m2.x][m2.y] != 0
+        ) {
+        return false;
+    }
+
+
+    // Make the modifications to the board state.
+    state[m0.x][m0.y] = 0;
+    state[m1.x][m1.y] = 0;
+    state[m2.x][m2.y] = 1;
+
+    return true;
+}
+
+// Recursively solve the board and add the found solutions to the output vector, 
+// and keep track of the current stack and the recursed level
+void Board::solve(vector<solution>& output, solution stack, int search_level) {
+    searched_amount++;
+    cout << searched_amount;
+    cout << "\r";
+
+
+    // Once we have recursed 35 moves, it means that the current move stack is a valid solution,
+    // because each move must eliminate a marble from the board, and there are 36 marbles.
+    if (search_level == MOVE_COUNT) {
+        output.push_back(stack);
+        cout << "\nFound solution: [\n";
+        for (int i = 0; i < MOVE_COUNT; i++) {
+            cout << static_cast<string>(stack[i]);
+            cout << "\n";
+        }
+        cout << "]\n";
+        return;
+    }
+
+    for (int x = 0; x < BOARD_SIZE; x++) {
+        for (int y = 0; y < BOARD_SIZE; y++) {
+            // Iterate over all marbles in the board in order to look for valid moves
+            if (state[x][y] == 1) {
+                // Look in all directions for a valid move
+                for (int dirInt = Up; dirInt < 4; dirInt++) {
+                    Dir dir = static_cast<Dir>(dirInt);
+                    // This created move does not have to be a valid move, but the X and Y coordinates of the 
+                    // move origin will always have a marble.
+                    Move mov;
+                    mov.x = x;
+                    mov.y = y;
+                    mov.dir = dir;
+                    // Copy the current state into another board instance so that the process can recurse
+                    Board m(state);
+                    // Try to make the move on the board. make_move will return true if the move successfully
+                    // completed, and if it did, then recurse
+                    if (m.make_move(mov)) {
+                        // Because the stack is passed by value, when trying different move origins 
+                        // and directions in the same search level, overwrite the current stack before passing it down
+                        stack[search_level] = mov;
+                        m.solve(output, stack, search_level + 1);
+                    }
+                }
+            }
+        }
+    }
+}
+
+int main()
+{
+    CONSOLE_CURSOR_INFO info;
+    info.dwSize = 100;
+    info.bVisible = false;
+    SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &info);
+
+    Board initial_state(initial_data);
+    solution stack = {};
+    vector<solution> solutions = {};
+    initial_state.solve(solutions, stack, 0);
+    cout << solutions.size();
+}
